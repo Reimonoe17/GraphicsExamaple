3323d7527a4f2bbe83cb596fdc7cefd75d31782d
Jamison Burton <jamison@burtontalk.com>
2022-04-19 08:00:02 -0600

    Add project files.

---
+++ GraphicsExamaple/GraphicsExamplesForm.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GraphicsExamplesForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SizeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CanvasPictureBox = New System.Windows.Forms.PictureBox()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.CanvasPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ColorToolStripMenuItem, Me.SizeToolStripMenuItem, Me.ClearToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(128, 100)
+        '
+        'ColorToolStripMenuItem
+        '
+        Me.ColorToolStripMenuItem.Name = "ColorToolStripMenuItem"
+        Me.ColorToolStripMenuItem.Size = New System.Drawing.Size(127, 32)
+        Me.ColorToolStripMenuItem.Text = "&Color"
+        '
+        'SizeToolStripMenuItem
+        '
+        Me.SizeToolStripMenuItem.Name = "SizeToolStripMenuItem"
+        Me.SizeToolStripMenuItem.Size = New System.Drawing.Size(127, 32)
+        Me.SizeToolStripMenuItem.Text = "&Size"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(127, 32)
+        Me.ClearToolStripMenuItem.Text = "C&lear"
+        '
+        'CanvasPictureBox
+        '
+        Me.CanvasPictureBox.BackColor = System.Drawing.SystemColors.Window
+        Me.CanvasPictureBox.Location = New System.Drawing.Point(428, 238)
+        Me.CanvasPictureBox.Name = "CanvasPictureBox"
+        Me.CanvasPictureBox.Size = New System.Drawing.Size(360, 200)
+        Me.CanvasPictureBox.TabIndex = 1
+        Me.CanvasPictureBox.TabStop = False
+        '
+        'GraphicsExamplesForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.Controls.Add(Me.CanvasPictureBox)
+        Me.Name = "GraphicsExamplesForm"
+        Me.Text = "Form1"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.CanvasPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SizeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CanvasPictureBox As PictureBox
+End Class
---
+++ GraphicsExamaple/GraphicsExamplesForm.vb
@@ -0,0 +1,135 @@
+﻿Public Class GraphicsExamplesForm
+
+    Dim currentColor As Color
+
+    Sub Sketch(startX As Integer, startY As Integer, endX As Integer, endY As Integer)
+        Dim g As Graphics = CanvasPictureBox.CreateGraphics
+        Dim pen As New Pen(Me.currentColor)
+        'Static Dim oldX, oldY As Integer
+
+        g.DrawLine(pen, startX, startY, endX, endY)
+        'oldX = currentX
+        'oldY = currentY
+
+        g.Dispose()
+        pen.Dispose()
+
+    End Sub
+
+    Sub Clear()
+        CanvasPictureBox.Refresh()
+    End Sub
+
+
+    Private Sub GraphicsExamplesForm_Click(sender As Object, e As EventArgs) Handles Me.Click
+        'DrawLine()
+        'DrawRectangle()
+        'DrawEllipse()
+        SineDraw()
+    End Sub
+
+    Sub DrawLine()
+        'instanciates new graphics object and tell it what to draw on
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Black)
+
+        ''draws a line from coordinate (0,0) to (100,100)
+        'g.DrawLine(pen, 0, 0, 100, 100)
+        'g.DrawLine(pen, 0, 0, Me.Width, Me.Height)
+        g.DrawLine(pen, 100, 100, 200, 200)
+
+        'clears out the data to keep from allocating storage
+        pen.Dispose()
+        g.Dispose()
+    End Sub
+
+    Sub DrawRectangle()
+        'instanciates new graphics object and tell it what to draw on
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Crimson)
+
+        pen.Width = 20
+
+        g.DrawRectangle(pen, 100, 100, 200, 300)
+
+
+        pen.Dispose()
+        g.Dispose()
+
+    End Sub
+
+    Sub DrawEllipse()
+        'instanciates new graphics object and tell it what to draw on
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Honeydew)
+
+        pen.Width = 20
+
+        g.DrawEllipse(pen, 100, 100, 200, 300)
+
+
+        pen.Dispose()
+        g.Dispose()
+    End Sub
+
+    Private Sub GraphicsExamplesForm_MouseMove(sender As Object, e As MouseEventArgs) Handles CanvasPictureBox.MouseMove
+        Static Dim oldX, oldY As Integer
+
+        Me.Text = $"({e.X},{e.Y}{e.Button.ToString()})"
+
+        Select Case e.Button.ToString
+            Case "Left"
+                Sketch(oldX, oldY, e.X, e.Y)
+            Case "Right"
+            Case "Middle"
+                'ColorDialog1.ShowDialog()
+                'Me.currentColor = ColorDialog1.Color
+                Clear()
+        End Select
+
+        oldX = e.X
+        oldY = e.Y
+
+    End Sub
+
+    Private Sub GraphicsExamplesForm_MouseDown(sender As Object, e As MouseEventArgs) Handles CanvasPictureBox.MouseDown
+        Me.Text = $"You are Pressing:  {e.Button.ToString()}"
+    End Sub
+
+    Private Sub GraphicsExamplesForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        currentColor = Color.Black
+    End Sub
+
+    Sub SineDraw()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Crimson)
+        Dim dcOffset As Integer = 0
+        Dim x As Integer
+        Dim y As Double
+        Dim oldX As Integer = 0
+        Dim oldY As Double = 100
+
+        pen.Width = 20
+
+        dcOffset = 100
+
+        For i = 0 To 360
+            x = i
+            'Vi = Vp * Sin(angle) +DcV
+            y = (100 * (Math.Sin(i * ((Math.PI) / 180)))) + dcOffset
+
+            g.DrawLine(pen, oldX, CInt(oldY), x, CInt(y))
+
+
+
+            oldX = x
+            oldY = y
+
+        Next
+
+        pen.Dispose()
+        g.Dispose()
+
+
+    End Sub
+End Class
